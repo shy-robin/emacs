7ec42d7f65b2bf32989126f69d898f0f35d48de1
ShyRobin <shy_robin@163.com>
2023-04-08 15:44:53 +0800

    feat: web mode

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -34,7 +34,8 @@
 (use-package js2-mode)
 (setq auto-mode-alist
       (append
-       '(("\\.js\\'" . js2-mode))
+       '(("\\.js\\'" . js2-mode)
+         ("\\.html\\'" .web-mode))
        auto-mode-alist))
 
 ;; nodejs
@@ -42,5 +43,33 @@
 
 ;; macOS 下快速在 finder 下打开文件
 (use-package reveal-in-osx-finder)
+
+(use-package web-mode)
+
+(defun my-web-mode-indent-setup()
+  ;; 设置 html、css、js 的缩进大小
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  )
+(add-hook 'web-mode-hook 'my-web-mode-indent-setup)
+
+;; 切换缩进
+(defun my-web-mode-toggle-indent()
+  (interactive)
+  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+    (progn (setq js-indent-level (if (= js-indent-level 2) 4 2))
+           (setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))
+    )
+  )
+  (if (eq major-mode 'web-mode)
+    (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+           (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+           (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))
+    )
+  )
+  (if (eq major-mode 'css-mode) (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+  (setq indent-tabs-mode nil)
+)
 
 (provide 'init-packages)
